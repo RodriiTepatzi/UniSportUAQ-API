38a8671695a708232fa61d61eff10e236ece3eb6
RodriiTepatzi <rodrif19@hotmail.com>
2024-02-12 14:01:01 -0600

    SPOR-19 adding the jwt auth endpoint

---
+++ Models/JWTRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace UniSportUAQ_API.Models
+{
+	public class JWTRequest
+	{
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using UniSportUAQ_API.Data;
 using UniSportUAQ_API.Data.Models;
 using UniSportUAQ_API.Data.Services;
+using UniSportUAQ_API.Models;
 
 namespace UniSportUAQ_API
 {
@@ -40,7 +46,65 @@ namespace UniSportUAQ_API
 				app.UseHsts();
 			}
 
-			app.UseHttpsRedirection();
+            app.MapPost("/security/createToken",
+            [AllowAnonymous]
+            async
+            (JWTRequest user) =>
+            {
+                if (!string.IsNullOrEmpty(user.Username) && !string.IsNullOrEmpty(user.Password))
+                {
+                    using (var serviceScope = app.Services.CreateScope())
+                    {
+                        var userManager = serviceScope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+                        var applicationUser = await userManager!.FindByNameAsync(user.Username);
+                        var id = await userManager.GetUserIdAsync(applicationUser);
+                        if (applicationUser != null)
+                        {
+                            var checkPwd = await userManager.CheckPasswordAsync(applicationUser, user.Password);
+                            if (!checkPwd)
+                            {
+                                return Results.Unauthorized();
+                            }
+                        }
+                        else
+                        {
+                            return Results.BadRequest("No user with that data exists.");
+                        }
+                    }
+                }
+                else
+                {
+                    return Results.BadRequest("All fields are needed.");
+                }
+
+                var issuer = builder.Configuration["Jwt:Issuer"];
+                var audience = builder.Configuration["Jwt:Audience"];
+                var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]!);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new[]
+                    {
+                        new Claim("Id", Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+                        new Claim(JwtRegisteredClaimNames.Email, user.Username),
+                        new Claim(JwtRegisteredClaimNames.Jti,
+                        Guid.NewGuid().ToString())
+                    }),
+                    Expires = DateTime.UtcNow.AddMinutes(15),
+                    Issuer = issuer,
+                    Audience = audience,
+                    SigningCredentials = new SigningCredentials
+                    (new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha512Signature)
+                };
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var jwtToken = tokenHandler.WriteToken(token);
+                var stringToken = tokenHandler.WriteToken(token);
+                return Results.Ok(stringToken);
+            });
+
+            app.UseHttpsRedirection();
 			app.UseStaticFiles();
 
 			app.UseRouting();
