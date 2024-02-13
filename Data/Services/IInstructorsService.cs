28ec2f9b70392cd9e8cf73564837c0174964092a
RodriiTepatzi <rodrif19@hotmail.com>
2024-02-13 11:53:24 -0600

    adding endpoint for instructors and fetch them by email

--- Data/Services/IInstructorsService.cs
+++ Data/Services/IInstructorsService.cs
@@ -7,5 +7,6 @@ namespace UniSportUAQ_API.Data.Services
 		Task<Instructor> CreateInstructorAsync(Instructor instructor);
 		Task<List<Instructor>> GetInstructorByIdAsync(string id);
         Task<List<Instructor>> GetInstructorByExpAsync(string id);
+        Task<List<Instructor>> GetInstructorByEmailAsync(string email)
     }
 }
