88acc2b9e1de6c064661578759a7bedd939e3af3
RodriiTepatzi <rodrif19@hotmail.com>
2024-02-13 12:12:45 -0600

    adding endpoints for the admins it also includes the SPOR9 and SPOR-10

---
+++ Data/Services/IAdminsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UniSportUAQ_API.Data.Models;
+
+namespace UniSportUAQ_API.Data.Services
+{
+	public interface IAdminsService
+	{
+		Task<List<Admin>> GetAdminByIdAsync(string id);
+        Task<List<Admin>> GetAdminByExpAsync(string exp);
+        Task<List<Admin>> GetAdminByEmailAsync(string email);
+    }
+}
+
