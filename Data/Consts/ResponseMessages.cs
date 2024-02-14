7dbb66f77e022b2cddc2d682bd6ef566f5eaa790
RodriiTepatzi <rodrif19@gmail.com>
2024-02-13 18:45:02 -0600

    adding response message

---
+++ Data/Consts/ResponseMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace UniSportUAQ_API.Data.Consts
+{
+	public class ResponseMessages
+	{
+		public const string OBJECT_NOT_FOUND = "Unable to locate the object in the database with the identifier you provided.";
+		public const string UNAUTHORIZED = "You do not have access to this endpoint.";
+		public const string BAD_REQUEST = "The data you provided was not properly provided. Please check our documentation.";
+		public const string INTERNAL_ERROR = "Internal error. Please contact and provide us some details about how this was generated.";
+	}
+}
