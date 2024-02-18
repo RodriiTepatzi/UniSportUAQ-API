48be1a7bccc3b5be898c48f50c4e209d88aac70b
rodrex25 <gabresh25gmail.com>
2024-02-18 15:09:51 -0600

    feat: added both schemas attendance course

---
+++ Data/Schemas/AttendanceSchema.cs
@@ -0,0 +1,12 @@
+﻿namespace UniSportUAQ_API.Data.Schemas
+{
+    public class AttendanceSchema
+    {
+        public string? Id { get; set; }
+        public string? StudentId { get; set; }
+        public string? CourseId { get; set; }
+        public bool? AttendanceClass { get; set; }
+
+
+    }
+}
