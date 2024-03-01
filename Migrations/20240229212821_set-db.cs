eff04e7d5dee6f79ac4114aa3e7286acab2cdf9d
rodrex25 <gabresh25gmail.com>
2024-02-29 18:02:58 -0600

    feat: attendances create added

--- Migrations/20240229212821_set-db.cs
+++ Migrations/20240229212821_set-db.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace UniSportUAQ_API.Migrations
 {
     /// <inheritdoc />
-    public partial class updating : Migration
+    public partial class setdb : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
