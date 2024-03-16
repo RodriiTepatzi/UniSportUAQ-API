adc86357f575cc1789627cf321c3b83490f64aa5
rdxUAQ <157053122+rdxUAQ@users.noreply.github.com>
2024-03-16 14:22:27 -0600

    feat: added in Data Models Class CartaLiberacion

---
+++ Data/Models/CartaLiberacion.cs
@@ -0,0 +1,40 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Scripting.Interpreter;
+
+
+
+namespace UniSportUAQ_API.Data.Models
+{
+    public class CartaLiberacion
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string? Id { get; set; }
+
+        [Required]
+        public string? StudentId { get; set; }
+
+        [Required]
+        public string? CourseId { get; set; }
+
+        [Required]
+        public string? Url { get; set; }
+
+
+        [ForeignKey("CourseId")]
+        public Course? Course { get; set; }
+
+        public Dictionary<string, object> Dictionary => new Dictionary<string, object> {
+
+            { nameof(Id), Id },
+            { nameof(StudentId), StudentId},
+            { nameof(CourseId), CourseId},
+            { nameof(Url), Url },
+
+        };
+    }
+}
+
