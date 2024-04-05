256c2fd67b286ab132da6eba8939231c88e135dc
RodriiTepatzi <rodrif19@gmail.com>
2024-04-05 15:07:21 -0600

    fix: fixing bug on inscription and also refactoring application table and class

---
+++ Migrations/20240405205624_fixing-users.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniSportUAQ_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixingusers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attendances_ApplicationUsers_StudentId",
+                table: "Attendances");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartasLiberacion_ApplicationUsers_StudentId",
+                table: "CartasLiberacion");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Inscriptions_ApplicationUsers_StudentId",
+                table: "Inscriptions");
+
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attendances_ApplicationUsers_StudentId",
+                table: "Attendances",
+                column: "StudentId",
+                principalTable: "ApplicationUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartasLiberacion_ApplicationUsers_StudentId",
+                table: "CartasLiberacion",
+                column: "StudentId",
+                principalTable: "ApplicationUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Inscriptions_ApplicationUsers_StudentId",
+                table: "Inscriptions",
+                column: "StudentId",
+                principalTable: "ApplicationUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attendances_ApplicationUsers_StudentId",
+                table: "Attendances");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartasLiberacion_ApplicationUsers_StudentId",
+                table: "CartasLiberacion");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Inscriptions_ApplicationUsers_StudentId",
+                table: "Inscriptions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CurrentCourseId",
+                table: "ApplicationUsers",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUsers_CurrentCourseId",
+                table: "ApplicationUsers",
+                column: "CurrentCourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApplicationUsers_Inscriptions_CurrentCourseId",
+                table: "ApplicationUsers",
+                column: "CurrentCourseId",
+                principalTable: "Inscriptions",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attendances_ApplicationUsers_StudentId",
+                table: "Attendances",
+                column: "StudentId",
+                principalTable: "ApplicationUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartasLiberacion_ApplicationUsers_StudentId",
+                table: "CartasLiberacion",
+                column: "StudentId",
+                principalTable: "ApplicationUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Inscriptions_ApplicationUsers_StudentId",
+                table: "Inscriptions",
+                column: "StudentId",
+                principalTable: "ApplicationUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
