51d58118d8593efbb40d2c02c7776d122dc4c223
rdxUAQ <157053122+rdxUAQ@users.noreply.github.com>
2024-04-07 16:33:01 -0600

    feat: inscriptions modified

--- Data/Models/Inscription.cs
+++ Data/Models/Inscription.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UniSportUAQ_API.Data.Models
@@ -7,13 +8,13 @@ namespace UniSportUAQ_API.Data.Models
     {
         [Required]
         [Key]
-		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		public string? Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string? Id { get; set; }
 
         [Required]
         public DateTime DateInscription { get; set; }
 
-        public bool Accredit { get; set; }
+
 
 
         [Required]
@@ -22,6 +23,14 @@ namespace UniSportUAQ_API.Data.Models
         [Required]
         public string? CourseId { get; set; }
 
+        public int Grade { get; set; }
+
+        public bool Accredit { get; set; }
+
+        [Required]
+        [DefaultValue(false)]
+        public bool IsFinished { get; set; }
+
 
         [ForeignKey("StudentId")]
         public ApplicationUser? Student { get; set; }
@@ -29,21 +38,23 @@ namespace UniSportUAQ_API.Data.Models
         [ForeignKey("CourseId")]
         public Course? Course { get; set; }
 
-		public Dictionary<string, object> ToDictionary() => new Dictionary<string, object>
-		{
-			{"Id", Id },
-			{"DateInscription", DateInscription },
-			{"Accredit", Accredit },
-			{"Student", Student is not null ? Student.ToDictionary : null },
-			{"Course", Course is not null ? Course.Dictionary : null  }
-		};
+        public Dictionary<string, object> ToDictionary() => new Dictionary<string, object>
+        {
+            { nameof(Id), Id is not null ? Id:"" },
+            { nameof(DateInscription), DateInscription },
+            { nameof(Student), Student is not null ? Student.ToDictionary : "" },
+            { nameof(Course), Course is not null ? Course.Dictionary : ""  },
+            { nameof(Grade), Grade},
+            { nameof(Accredit), Accredit},
+            { nameof(IsFinished), IsFinished}
+        };
 
-		public Dictionary<string, object> ToDictionaryWithNoStudent() => new Dictionary<string, object>
-		{
-			{"Id", Id },
-			{"DateInscription", DateInscription },
-			{"Accredit", Accredit },
-			{"Course", Course is not null ? Course.Dictionary : null  }
-		};
-	}
+        public Dictionary<string, object> ToDictionaryWithNoStudent() => new Dictionary<string, object>
+        {
+            {"Id", Id },
+            {"DateInscription", DateInscription },
+            {"Accredit", Accredit },
+            {"Course", Course is not null ? Course.Dictionary : null  }
+        };
+    }
 }
