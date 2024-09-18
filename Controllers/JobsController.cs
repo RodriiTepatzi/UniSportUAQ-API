22bcc45d8f9b6818e2f3ed496349765e22044fec
rdxUAQ <gmorales37@alumnos.uaq.mx>
2024-09-18 16:14:51 -0600

    #58

---
+++ Controllers/JobsController.cs
@@ -0,0 +1,22 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UniSportUAQ_API.Controllers
+{
+    [Route("api/v1/hangfire")]
+    [ApiController]
+    public class JobsController : Controller
+    {
+        [HttpPost]
+        [Route("simpletask")]
+        [AllowAnonymous]
+        public ActionResult CreateBackgroundJob() {
+
+            BackgroundJob.Enqueue(() => Console.WriteLine("Its working"));
+            return Ok();
+        
+        }
+    }
+}
