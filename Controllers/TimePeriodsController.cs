934cff0d04d4983b751fb0a1b1c82a88a28e92b7
rdxUAQ <gmorales37@alumnos.uaq.mx>
2024-10-09 09:15:12 -0600

    #113

--- Controllers/TimePeriodsController.cs
+++ Controllers/TimePeriodsController.cs
@@ -3,10 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using UniSportUAQ_API.Data.Interfaces;
 using UniSportUAQ_API.Data.Models;
 using UniSportUAQ_API.Data.Consts;
-using System.Linq;
-using Microsoft.AspNetCore.Identity;
 using UniSportUAQ_API.Data.Schemas;
-using UniSportUAQ_API.Data.Services;
+using UniSportUAQ_API.Data.Base;
+using UniSportUAQ_API.Data.DTO;
 
 namespace UniSportUAQ_API.Controllers
 {
@@ -20,7 +19,7 @@ namespace UniSportUAQ_API.Controllers
 
         public TimePeriodsController(ITimePeriodsService timePeriodsService) {
 
-            this._timePeriodsService = timePeriodsService;
+            _timePeriodsService = timePeriodsService;
 
 
         }
@@ -28,125 +27,170 @@ namespace UniSportUAQ_API.Controllers
 
         //get id
         [HttpGet]
-        [Route("/id/{id}")]
+        [Route("id/{id}")]
         [Authorize]
 
-        public async Task<IActionResult> GetTimePeriodById(string id) {
+        public async Task<IActionResult> GetTimePeriodById(string id)
+        {
 
-            if (!Guid.TryParse(id, out _)) return BadRequest(new DataResponse {Data= null, ErrorMessage = ResponseMessages.BAD_REQUEST });
+            if (!Guid.TryParse(id, out _)) return BadRequest(new BaseResponse<TimePeriodDTO> { Error = ResponseErrors.DataNotFound });
 
             var result = await _timePeriodsService.GetByIdAsync(id);
 
-            if(result == null) return Ok(new DataResponse { Data = null, ErrorMessage = ResponseMessages.OBJECT_NOT_FOUND});
+            if (result == null) return Ok(new BaseResponse<TimePeriodDTO> { Error = ResponseErrors.DataNotFound });
+
+            var timePeriodDTO = new TimePeriodDTO
+            {
+                Id = result.Id,
+                Period = result.Period,
+                Type = result.Type,
+                DateStart = result.DateStart,
+                DateEnd = result.DateEnd,
+            };
 
             //change to dto
-            return Ok(new DataResponse { Data = result.Dictionary, ErrorMessage = null});
+            return Ok(new BaseResponse<TimePeriodDTO> { Data = timePeriodDTO });
         }
 
         //get period
         [HttpGet]
-        [Route("/period/{period}")]
+        [Route("period/{period}")]
         [Authorize]
-        public async Task<IActionResult> GetTimePeriodByPeriod(string period) { 
+        public async Task<IActionResult> GetTimePeriodByPeriod(string period)
+        {
 
-            if(string.IsNullOrEmpty(period)) return BadRequest(new DataResponse { Data = null, ErrorMessage = ResponseMessages.BAD_REQUEST });
+            if (string.IsNullOrEmpty(period)) return BadRequest(new BaseResponse<List<TimePeriodDTO>> { Error = ResponseErrors.AttributeEmptyOrNull });
 
             var result = await _timePeriodsService.GetAllAsync(a => a.Period == period);
 
 
-            var Data = new List<Dictionary<string, object>>();
+            var Data = new List<TimePeriodDTO>();
 
-            if (result.Count() < 1)  return Ok(new DataResponse { Data = null, ErrorMessage = ResponseMessages.OBJECT_NOT_FOUND });
+            if (result.Count() < 1) return Ok(new BaseResponse<bool> { Data = false, Error = ResponseErrors.EntityNotExist });
 
-            foreach (var kvp in result) {
-
-                Data.Add(kvp.Dictionary);
-
+            foreach (var kvp in result)
+            {
+                var timePeriodDTO = new TimePeriodDTO
+                {
+                    Id = kvp.Id,
+                    Period = kvp.Period,
+                    Type = kvp.Type,
+                    DateStart = kvp.DateStart,
+                    DateEnd = kvp.DateEnd,
+                };
+                Data.Add(timePeriodDTO);
             }
 
-            return Ok(new DataResponse { Data = Data, ErrorMessage = null });
+            return Ok(new BaseResponse<List<TimePeriodDTO>> { Data = Data, Error = ResponseErrors.DataNotFound });
         }
-
 
         //get type
         [HttpGet]
-        [Route("/type/{type}")]
+        [Route("type/{type}")]
         [Authorize]
 
-        public async Task<IActionResult> GetTimePeriodByType(string type) {
+        public async Task<IActionResult> GetTimePeriodByType(string type)
+        {
 
-            if (string.IsNullOrEmpty(type)) return BadRequest(new DataResponse { Data = null, ErrorMessage = ResponseMessages.BAD_REQUEST });
-            
-            var result =await  _timePeriodsService.GetAllAsync(a => a.Type == type);
+            if (string.IsNullOrEmpty(type)) return BadRequest(new BaseResponse<List<TimePeriodDTO>> { Error = ResponseErrors.AttributeEmptyOrNull });
 
-            var Data = new List<Dictionary<string, object>>();
+            var result = await _timePeriodsService.GetAllAsync(a => a.Type == type);
 
-            if(result.Count() < 1) return Ok(new DataResponse { Data = null, ErrorMessage = ResponseMessages.OBJECT_NOT_FOUND });
+            var Data = new List<TimePeriodDTO>();
 
-            foreach (var kvp in result) {
+            if (result.Count() < 1) return Ok(new BaseResponse<bool> { Data = false, Error = ResponseErrors.EntityNotExist });
 
-                Data.Add(kvp.Dictionary);
+            foreach (var kvp in result)
+            {
+                var timePeriodDTO = new TimePeriodDTO
+                {
+                    Id = kvp.Id,
+                    Period = kvp.Period,
+                    Type = kvp.Type,
+                    DateStart = kvp.DateStart,
+                    DateEnd = kvp.DateEnd,
+                };
 
+                Data.Add(timePeriodDTO);
             }
 
-            return Ok(new DataResponse { Data = Data, ErrorMessage = null });
+            return Ok(new BaseResponse<List<TimePeriodDTO>> { Data = Data, Error = ResponseErrors.DataNotFound });
         }
 
-        //get date start
+
         [HttpGet]
-        [Route("/datestart")]
+        [Route("datestart")]
         [Authorize]
 
-        public async Task<IActionResult> GetTimePeriodByDateStart(string date) { 
+        public async Task<IActionResult> GetTimePeriodByDateStart(string date)
+        {
 
-            if(!DateTime.TryParse(date, out _)) return BadRequest(new DataResponse { Data = null, ErrorMessage = "date: "+ResponseMessages.BAD_REQUEST });
+            if (!DateTime.TryParse(date, out _)) return BadRequest(new BaseResponse<TimePeriodDTO> { Error = ResponseErrors.DataNotFound });
 
             DateTime dateTime = DateTime.Parse(date).Date;
 
             var result = await _timePeriodsService.GetAllAsync(a => a.DateStart.Date == dateTime);
 
-            var Data = new List<Dictionary<string, object>>();
+            var Data = new List<TimePeriodDTO>();
 
-            if (result.Count() < 1) return Ok(new DataResponse { Data = null, ErrorMessage = ResponseMessages.OBJECT_NOT_FOUND });
+            if (result.Count() < 1) return Ok(new BaseResponse<bool> { Data = false, Error = ResponseErrors.EntityNotExist });
 
             foreach (var kvp in result)
             {
+                var timePeriodDTO = new TimePeriodDTO
+                {
+                    Id = kvp.Id,
+                    Period = kvp.Period,
+                    Type = kvp.Type,
+                    DateStart = kvp.DateStart,
+                    DateEnd = kvp.DateEnd,
+                };
 
-                Data.Add(kvp.Dictionary);
+                Data.Add(timePeriodDTO);
 
             }
 
-            return Ok(new DataResponse { Data = Data, ErrorMessage = null });
-
+            return Ok(new BaseResponse<List<TimePeriodDTO>> { Data = Data, Error = ResponseErrors.DataNotFound });
         }
+
+        //get date end
         //get date end
         [HttpGet]
-        [Route("/dateend")]
+        [Route("dateend/{date}")]
         [Authorize]
 
         public async Task<IActionResult> GetTimePeriodByDateEnd(string date)
         {
 
-            if (!DateTime.TryParse(date, out _)) return BadRequest(new DataResponse { Data = null, ErrorMessage = "date: " + ResponseMessages.BAD_REQUEST });
+            if (!DateTime.TryParse(date, out _)) return BadRequest(new BaseResponse<TimePeriodDTO> { Error = ResponseErrors.DataNotFound });
 
             DateTime dateTime = DateTime.Parse(date).Date;
 
             var result = await _timePeriodsService.GetAllAsync(a => a.DateEnd.Date == dateTime);
 
-            var Data = new List<Dictionary<string, object>>();
+            var Data = new List<TimePeriodDTO>();
 
-            if (result.Count() < 1) return Ok(new DataResponse { Data = null, ErrorMessage = ResponseMessages.OBJECT_NOT_FOUND });
+            if (result.Count() < 1) return Ok(new BaseResponse<bool> { Data = false, Error = ResponseErrors.EntityNotExist });
 
             foreach (var kvp in result)
             {
 
-                Data.Add(kvp.Dictionary);
+                var timePeriodDTO = new TimePeriodDTO
+                {
+                    Id = kvp.Id,
+                    Period = kvp.Period,
+                    Type = kvp.Type,
+                    DateStart = kvp.DateStart,
+                    DateEnd = kvp.DateEnd,
+                };
+
+                Data.Add(timePeriodDTO);
 
             }
 
-            return Ok(new DataResponse { Data = Data, ErrorMessage = null });
-
+            return Ok(new BaseResponse<List<TimePeriodDTO>> { Data = Data, Error = ResponseErrors.DataNotFound });
         }
+
 
         //post 
         [HttpPost]
@@ -156,19 +200,20 @@ namespace UniSportUAQ_API.Controllers
         {
 
             //validate register attrributes
-            if (string.IsNullOrEmpty(period.Period)) return BadRequest(new DataResponse { Data = null, ErrorMessage = ResponseMessages.BAD_REQUEST });
-            if (string.IsNullOrEmpty(period.Type)) return BadRequest(new DataResponse { Data = null, ErrorMessage = ResponseMessages.BAD_REQUEST });
+            if (string.IsNullOrEmpty(period.Period)) return BadRequest(new BaseResponse<bool> { Error = ResponseErrors.AttributeIdInvalidlFormat });
+            if (string.IsNullOrEmpty(period.Type)) return BadRequest(new BaseResponse<bool> { Error = ResponseErrors.AttributeIdInvalidlFormat });
             //revew period start end no colition
-            if(period.DateStart >= period.DateEnd) return BadRequest(new DataResponse { Data = null, ErrorMessage = ResponseMessages.BAD_REQUEST });
+            if (period.DateStart >= period.DateEnd) return BadRequest(new BaseResponse<bool> { Error = ResponseErrors.CourseHorarioConfict });
 
 
             //check exist
             var result = await _timePeriodsService.GetAllAsync();
 
-            foreach (var per in result) {
-                if (per.Period!.ToLower() == period.Period.ToLower()) return Ok(new DataResponse { Data = null, ErrorMessage = ResponseMessages.ENTITY_EXISTS });
-                if (IsScheduleConflict(per.DateStart, per.DateEnd, period.DateStart, period.DateEnd)) return Ok(new DataResponse { Data = null, ErrorMessage = ResponseMessages.PERIOD_CROSSED });
-                
+            foreach (var per in result)
+            {
+                if (per.Period!.ToLower() == period.Period.ToLower()) return Ok(new BaseResponse<bool> { Error = ResponseErrors.EntityExist });
+                if (IsScheduleConflict(per.DateStart, per.DateEnd, period.DateStart, period.DateEnd)) return Ok(new BaseResponse<bool> { Error = ResponseErrors.CourseHorarioConfict });
+
             }
 
             //if (result.Any()) return Ok(new DataResponse { Data = null, ErrorMessage = ResponseMessages.ENTITY_EXISTS });
@@ -185,9 +230,9 @@ namespace UniSportUAQ_API.Controllers
 
             var periodAdded = await _timePeriodsService.AddAsync(newPeriod);
 
-            if (periodAdded is not null) return Ok(new DataResponse { Data = periodAdded.Dictionary, ErrorMessage = null });
+            if (periodAdded is not null) return Ok(new BaseResponse<bool> { Error = ResponseErrors.AttributeEmptyOrNull });
 
-            return BadRequest(new DataResponse { Data = null, ErrorMessage = ResponseMessages.INTERNAL_ERROR });
+            return Ok(new BaseResponse<bool> { Data = true });
         }
 
         //put
@@ -198,23 +243,23 @@ namespace UniSportUAQ_API.Controllers
         public async Task<IActionResult> UpdateTimePeriodAsync([FromBody] TimePeriodsSchema timePeriodSchema)
         {
             //validation
-            if (string.IsNullOrEmpty(timePeriodSchema.Id)) return BadRequest(new DataResponse { Data = null, ErrorMessage = ResponseMessages.BAD_REQUEST });
-            if (string.IsNullOrEmpty(timePeriodSchema.Period)) return BadRequest(new DataResponse { Data = null, ErrorMessage = ResponseMessages.BAD_REQUEST });
-            if (string.IsNullOrEmpty(timePeriodSchema.Type)) return BadRequest(new DataResponse { Data = null, ErrorMessage = ResponseMessages.BAD_REQUEST });
+            if (string.IsNullOrEmpty(timePeriodSchema.Id)) return BadRequest(new BaseResponse<bool> { Error = ResponseErrors.AttributeEmptyOrNull });
+            if (string.IsNullOrEmpty(timePeriodSchema.Period)) return BadRequest(new BaseResponse<bool> { Error = ResponseErrors.AttributeEmptyOrNull });
+            if (string.IsNullOrEmpty(timePeriodSchema.Type)) return BadRequest(new BaseResponse<bool> { Error = ResponseErrors.AttributeEmptyOrNull });
 
             //search exist entity
             var result = await _timePeriodsService.GetAllAsync();
             var oldTimePeriod = result.FirstOrDefault(per => per.Id == timePeriodSchema.Id);
 
-            if (oldTimePeriod == null) return BadRequest(new DataResponse { Data = null, ErrorMessage = ResponseMessages.BAD_REQUEST });
+            if (oldTimePeriod == null) return Ok(new BaseResponse<bool> { Error = ResponseErrors.EntityNotExist });
 
             //verify dates
             foreach (var per in result)
             {
                 if (per.Period!.ToLower() == timePeriodSchema.Period.ToLower() && per.Id != oldTimePeriod.Id)
-                    return Ok(new DataResponse { Data = null, ErrorMessage = ResponseMessages.ENTITY_EXISTS });
+                    return Ok(new BaseResponse<bool> { Error = ResponseErrors.EntityExist });
                 if (IsScheduleConflict(per.DateStart, per.DateEnd, timePeriodSchema.DateStart, timePeriodSchema.DateEnd) && per.Id != oldTimePeriod.Id)
-                    return Ok(new DataResponse { Data = null, ErrorMessage = ResponseMessages.PERIOD_CROSSED });
+                    return Ok(new BaseResponse<bool> { Error = ResponseErrors.EntityExist });
             }
 
             //update existing entity without creating a new instance
@@ -226,11 +271,10 @@ namespace UniSportUAQ_API.Controllers
             //update entity
             var newPeriod = await _timePeriodsService.UpdateAsync(oldTimePeriod);
 
-            if (newPeriod is null) return Ok(new DataResponse { Data = null, ErrorMessage = ResponseMessages.ERROR_PROMOTING });
+            if (newPeriod is null) return Ok(new BaseResponse<bool> { Error = ResponseErrors.SysErrorPromoting });
 
-            return Ok(new DataResponse { Data = newPeriod.Dictionary, ErrorMessage = null });
+            return Ok(new BaseResponse<bool> { Data = true });
         }
-
         private bool IsScheduleConflict(DateTime existingStart, DateTime existingEnd, DateTime newStart, DateTime newEnd)
         {
                 if (existingStart < newEnd && newStart < existingEnd)
