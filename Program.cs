d16e0bae3f7c80cea96f156f90c3579b655011f0
Marco Rodrigo Flores <rodrigo.flores@dgmxtech.com>
2024-10-22 15:48:46 -0600

    adding env

---
+++ Program.cs
@@ -0,0 +1,187 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using UniSportUAQ_API.Data;
+using UniSportUAQ_API.Data.Interfaces;
+using UniSportUAQ_API.Data.Models;
+using UniSportUAQ_API.Data.Services;
+using UniSportUAQ_API.Models;
+using Hangfire;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using UniSportUAQ_API.Controllers;
+using UniSportUAQ_API.Hubs;
+using Hangfire.Dashboard;
+using Microsoft.Extensions.Options;
+using Hangfire.MySql;
+using System.Transactions;
+using DotNetEnv;
+
+namespace UniSportUAQ_API
+{
+    public class Program
+    {
+        public static IConfiguration? Configuration { get; private set; }
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            Configuration = builder.Configuration;
+            Env.Load();
+
+            var mysqlConnectionString = Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING");
+            var jwtIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER");
+            var jwtAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE");
+            var jwtKey = Environment.GetEnvironmentVariable("JWT_KEY");
+
+            builder.Services.AddDbContext<AppDbContext>(
+                options => options.UseMySQL(
+                    mysqlConnectionString!,
+                    providerOptions => providerOptions.EnableRetryOnFailure()
+                ));
+
+
+            builder.Services.AddCors(options =>
+			{
+				options.AddPolicy("AllowAll",
+					builder =>
+					{
+						builder.WithOrigins("https://localhost:port", "https://deportetroyanos.azurewebsites.net")
+							   .AllowAnyHeader()
+							   .AllowAnyMethod()
+							   .AllowCredentials();  // Importante para SignalR
+					});
+			});
+
+			builder.Services.AddSignalR();
+
+			builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+				.AddEntityFrameworkStores<AppDbContext>();
+
+			builder.Services.AddMemoryCache();
+
+			builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = jwtIssuer,
+                    ValidAudience = jwtAudience,
+                    IssuerSigningKey = new SymmetricSecurityKey
+                    (Encoding.UTF8.GetBytes(jwtKey!)),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ClockSkew = TimeSpan.Zero,
+				};
+			});
+
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            });
+
+            builder.Services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+            builder.Services.AddTransient<IStudentsService, StudentsService>();
+            builder.Services.AddTransient<IInstructorsService, InstructorsService>();
+            builder.Services.AddTransient<IAdminsService, AdminsService>();
+            builder.Services.AddTransient<IUsersService, UsersService>();
+
+            builder.Services.AddTransient<ICoursesService, CoursesService>();
+            builder.Services.AddTransient<IInscriptionsService, InscriptionsService>();
+            builder.Services.AddTransient<IAttendancesService, AttendancesService>();
+            builder.Services.AddTransient<ICartasLiberacionService, CartasLiberacionService>();
+            builder.Services.AddTransient<ISubjectsService, SubjectsService>();
+            builder.Services.AddTransient<IUtilsService, UtilsService>();
+            builder.Services.AddTransient<ITimePeriodsService, TimePeriodsService>();
+            builder.Services.AddTransient<IHorariosService, HorariosService>();
+            builder.Services.AddTransient<IHangfireJobsService, HangfireJobsService>();
+
+            builder.Services.AddControllers();
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddHangfire((sp, config) =>
+            {
+                config.UseStorage(new MySqlStorage(mysqlConnectionString, new MySqlStorageOptions
+                {
+                    TransactionIsolationLevel = IsolationLevel.ReadCommitted,
+                    QueuePollInterval = TimeSpan.FromSeconds(15),
+                    JobExpirationCheckInterval = TimeSpan.FromHours(1),
+                    CountersAggregateInterval = TimeSpan.FromMinutes(5),
+                    PrepareSchemaIfNecessary = true,
+                    DashboardJobListLimit = 50000,
+                    TransactionTimeout = TimeSpan.FromMinutes(1),
+                    TablesPrefix = "Hangfire"
+                }));
+            });
+
+            builder.Services.AddHangfireServer();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting(); // Setup routing
+
+			app.UseCors("AllowAll");
+
+			app.UseSwagger(); // Swagger documentation
+			app.UseSwaggerUI();
+
+			// JWT Authentication middleware
+			app.UseAuthentication(); // Handle JWT tokens
+			app.UseAuthorization();  // Authorization middleware
+
+			// Map SignalR Hub after authentication is configured
+			app.MapHub<LessonHub>("/hubs/lessons"); // SignalR
+
+			// Map default route
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+
+			// Configure Hangfire dashboard
+			app.UseHangfireDashboard("/hangfire", new DashboardOptions
+			{
+				Authorization = new IDashboardAuthorizationFilter[] { }
+			});
+
+			// DatabaseInitializer.FeedUsersAndRoles(app);
+			// DatabaseInitializer.FeedDatabase(app);
+			// DatabaseInitializer.FeedInscriptions(app);
+			// DatabaseInitializer.FeedAttendances(app);
+
+			app.Run();
+		}
+    }
+}
--- README.md
+++ README.md
@@ -1,19 +1,9 @@
-Install-Package Microsoft.AspNetCore.OpenApi -Version 7.0.15
-Install-Package System.Text.Encoding.CodePages -Version 8.0.0
-Install-Package Microsoft.Extensions.Identity.Core -Version 7.0.15
-Install-Package Microsoft.EntityFrameworkCore -Version 7.0.15
-Install-Package Microsoft.EntityFrameworkCore.SqlServer -Version 7.0.15
-Install-Package consola -Version 0.2.3
-Install-Package FirebaseStorage.net -Version 1.0.3
-Install-Package itextsharp.xmlworker -Version 5.5.13.4
-Install-Package iTextSharp -Version 5.5.13.4
-Install-Package Microsoft.AspNetCore.Authentication.JwtBearer -Version 7.0.15
-Install-Package Microsoft.AspNetCore.Identity.EntityFrameworkCore -Version 7.0.15
-Install-Package Swashbuckle.AspNetCore -Version 6.5.0
-Install-Package Firebase.Auth -Version 1.0.0
-Install-Package Newtonsoft.Json -Version 13.0.3
-Install-Package Microsoft.EntityFrameworkCore.Tools -Version 7.0.15
-Install-Package Hangfire.Core
-Install-Package Hangfire.SqlServer
-Install-Package Hangfire.AspNetCore
-Install-Package Microsoft.Data.SqlClient
+Env Example
+
+# Default Connection Strings
+MYSQL_CONNECTION_STRING=Server=192.168.32.10;Port=3306;Database=deportes_uaq;User=root;Password=root;
+# JWT Settings
+JWT_ISSUER='issue'
+JWT_AUDIENCE='audience'
+JWT_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJc3N1ZXIgKGlzcykiOiJJc3N1ZXIiLCJJc3N1ZWQgQXQgKGlhdCkiOiIyMDI0LTAyLTE0VDAxOjUyOjAzLjc1MloiLCJFeHBpcmF0aW9uIFRpbWUgKGV4cCkiOiIyMDI0LTAyLTE0VDAyOjUyOjAzLjc1MloiLCJTdWJqZWN0IChzdWIpIjoiU3ViamVjdCIsIlVzZXJuYW1lIChhdWQpIjoiSmF2YUd1aWRlcyIsIlJvbGUiOiJBRE1JTiJ9.VoFvJXk8ZM9SqB8K6E-89TjAWfS-dTwhD6NufQlin9c'
+JWT_EXPIRES=60
